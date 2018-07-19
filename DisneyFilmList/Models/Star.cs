6e0d239993b958fbeb4cdd460be604f61c052e16
Ted Jacobs <doldrone@bellsouth.net>
2018-07-18 22:06:13 -0400

    In "Star" removed movie and producer.

--- DisneyFilmList/Models/Star.cs
+++ DisneyFilmList/Models/Star.cs
@@ -12,7 +12,7 @@ namespace DisneyFilmList.Models
         public int StarID { get; set; }
         public string Name { get; set; }
 
-        public virtual ICollection<Producer> Producers { get; set; }
-        public virtual ICollection<Movie> Movies { get; set; }
+        //public virtual ICollection<Producer> Producers { get; set; }
+        //public virtual ICollection<Movie> Movies { get; set; }
     }
 }
