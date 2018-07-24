542f3933ea9f3db9fb8efcf9a0144d580878cde6
Ted Jacobs <doldrone@bellsouth.net>
2018-07-24 18:33:04 -0400

    Added complete film list to "FilmListInitializer".

--- DisneyFilmList/DAL/FilmListInitializer.cs
+++ DisneyFilmList/DAL/FilmListInitializer.cs
@@ -10,7 +10,7 @@ namespace DisneyFilmList.DAL
         {
             var movies = new List<Movie>()
             {
-            new Movie {Title="White and the Seven Dwarfs",ReleaseDate=DateTime.Parse("1937-12-21"), Star = new Star{ Name="Adriana Caselotti" }, Producer = new Producer{Name="Walt Disney"}},
+            new Movie {Title="Snow White and the Seven Dwarfs",ReleaseDate=DateTime.Parse("1937-12-21"), Star = new Star{ Name="Adriana Caselotti" }, Producer = new Producer{Name="Walt Disney"}},
             new Movie {Title="Pinocchio",ReleaseDate=DateTime.Parse("1940-02-07"), Star = new Star{ Name="Dick Jones" }, Producer = new Producer{Name="Walt Disney"}},
             new Movie {Title="Fantasia",ReleaseDate=DateTime.Parse("1940-11-13"), Star = new Star{ Name="Leopold Stokowski" }, Producer = new Producer{Name="Walt Disney"}},
             new Movie {Title="Dumbo",ReleaseDate=DateTime.Parse("1941-10-23"), Star = new Star{ Name="Edward Brophy" }, Producer = new Producer{Name="Walt Disney"}},
